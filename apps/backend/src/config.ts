0187f519c61914affe5a163cbdff8d67aea7ddb1
Felix Sebastian <hi@felixsebastian.dev>
2025-12-18 14:17:50 +1100

    wip

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,4 +1,4 @@
 export const env = process.env.APP_ENV ?? "production";
 export const isProd = env === "production";
 export const isDev = env === "development";
-export const port = parseInt(process.env.BACKEND_PORT!, 10);
+export const port = parseInt(process.env.PORT ?? process.env.BACKEND_PORT ?? "3000", 10);
